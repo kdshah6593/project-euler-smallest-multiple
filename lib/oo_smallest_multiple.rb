29ba97ce0351054c698de758293817f937b1b4b4
Kunal <kdshah6593@gmail.com>
2021-08-17 12:41:47 -0400

    completed solution, tests passing

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,24 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+    attr_reader :lcm
+    
+    def initialize(num)
+        @lcm = find_multiple(num)
+    end
+
+    def find_multiple(num)
+        x = 1
+        start = 1
+        finish = num
+    
+        while start <= finish
+            if x % start == 0
+                start += 1
+            else
+                x += 1
+                start = 1
+            end
+        end
+        x
+    end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+def smallest_multiple(num)
+    x = 1
+    start = 1
+    finish = num
+
+    while start <= finish
+        if x % start == 0
+            start += 1
+        else
+            x += 1
+            start = 1
+        end
+    end
+    x
+end
